8c53ff4e0423bf9a3bffb16967c72a1f3ebdc719
万凯 <wankai@tech.com>
2016-03-30 20:34:59 +0800

    1

--- 一心一田/ShopcarViewController.m
+++ 一心一田/ShopcarViewController.m
@@ -77,6 +77,7 @@
 -(void)refreshgoodnum:(NSNotification *)anote{
     if (needrefresh) {
         [self initparas];
+        return;
     }
     
     NSArray *idsneedtorefresh=[[anote userInfo] arrayForKey:@"id"];
